324dd8f7b6ea1701e4881f043a89d5c3a34f202f
Martin Steiger <git@martin-steiger.de>
2013-08-25 13:31:06 +0200

    Reading volume/player status from MPDPlayer

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,30 +1,5 @@
 $(function() 
-{
-	$("#slider-range-min").slider(
-	{
-		range : "min",
-		value : 40,
-		min : 0,
-		step : 5,
-		max : 100,
-		slide : function(event, ui) 
-		{
-			$('#volume').html(ui.value);
-
-			jsRoutes.controllers.Application.volume(ui.value).ajax(
-			{
-				error : function(data) 
-				{
-					alert("AJAX call to volume(int amount) failed ");
-				}
-			});
-		}
-	});
-
-	$("#volume").html($("#slider-range-min").slider("value"));
-	
-	// for some reason this has to be inside the magical $(function()) scope
-	
+{	
 	// Add onClick handler for all playlist table rows
 	$('#playlist-table').on('click', 'tbody tr', function(event) 
 	{

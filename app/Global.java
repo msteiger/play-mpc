6af4f54713a91ab960ee6b05fe17bc7b9bdf293f
Martin Steiger <git@martin-steiger.de>
2013-09-08 18:08:50 +0200

    Removed Database setup

--- app/Global.java
+++ app/Global.java
@@ -21,12 +21,6 @@ public class Global extends GlobalSettings
 	@Override
 	public void onStart(Application app)
 	{
-		// Check if the database is empty
-		if (app.isDev() && User.count() == 0)
-		{
-			Logger.debug("Bootstrapping with default data");
-			Ebean.save((List<?>) Yaml.load("initial-data.yml"));
-		}
 	}
 	
 	@Override
